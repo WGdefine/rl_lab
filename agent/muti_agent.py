de2318de060fbb57b533e6b655c9f9e5c0abbc77
WG <44030930+WGdefine@users.noreply.github.com>
2023-04-28 17:02:23 +0800

    Initial commit

---
+++ agent/muti_agent.py
@@ -0,0 +1,67 @@
+import importlib
+from base_agent import BaseAgent
+import os
+import argparse
+
+
+def ini_agents(args):
+    # agent_file_path=os.getcwd()+"\\agents"
+    # print("agent path:{}".format(agent_file_path))
+    agent_file_name = str("agents.algo." + str(args.algo))
+    agent_file_import = importlib.import_module(agent_file_name)
+    agent_class_name = args.algo.upper()
+
+    # 实例化agent
+    agent = getattr(agent_file_import, agent_class_name)(args)
+    return agent
+
+
+class MultiRLAgents(BaseAgent):
+    def __init__(self, args):
+        super(MultiRLAgents, self).__init__(args)
+        self.args = args
+        self.algo = ini_agents(args)
+        self.set_agent()
+
+    def set_agent(self):
+        self.agent = self.algo.agents
+
+    def action_from_algo_to_env(self, joint_action, id):
+        """
+        :param joint_action:
+        :return: wrapped joint action: one-hot
+        """
+
+        joint_action_ = None
+        action_a = joint_action["action"]
+        if not self.args.action_continuous:  # discrete action space
+            each = [0] * self.args.action_space[id]
+            each[action_a] = 1
+            joint_action_ = each
+        else:
+            joint_action_ = action_a  # continuous action space
+
+        return joint_action_
+
+    def choose_action_to_env(self, observation, train=True):
+        obs_copy = observation.copy()
+        obs = obs_copy["obs"]
+        agent_id = obs_copy["controlled_player_index"]
+        action_from_algo = self.agent[agent_id].choose_action(obs, train)
+        action_to_env = self.action_from_algo_to_env(
+            {"action": action_from_algo}, agent_id
+        )
+        return action_to_env
+
+    def learn(self):
+        self.algo.learn()
+
+    def save(self, p_dir, epoch):
+        self.algo.save(p_dir, epoch)
+
+if __name__=="__main__":
+    param=argparse.ArgumentParser()
+    param.add_argument("--algo", default="msac", type=str)
+    arg=param.parse_args()
+    print("param:{}".format(arg))
+    ini_agents(arg)
